d1d634bb0c3e03588c1a0527affbf230ceb4f1dd
Lilly <103372583+LillyPK@users.noreply.github.com>
2026-04-28 02:41:25 -0400

    Add files via upload

---
+++ collections/network_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+sudo pacman -Syu --noconfirm
+
+sudo pacman -S --needed --noconfirm \
+    networkmanager \
+    network-manager-applet \
+    networkmanager-openvpn \
+    wireless-regdb \
+    ufw
+
+sudo systemctl enable --now NetworkManager
+sudo systemctl enable --now ufw
