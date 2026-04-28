6157952558bae97fcb3dd4952634fcf337a8c6d1
Lilly <103372583+LillyPK@users.noreply.github.com>
2026-04-28 02:02:22 -0400

    Add files via upload

---
+++ collections/software_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+sudo pacman -Syu --noconfirm
+
+sudo pacman -S --needed --noconfirm tmux cava
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+source "$HOME/.cargo/env"
+
+curl -fsSL https://raw.githubusercontent.com/LillyPK/bsdmp/refs/heads/master/install.sh | bash
